d2805d43ee622fd05fa7a3452003e80b7eba6da1
gokhan.guzelyurt <Gok26han@>
2022-02-01 09:49:06 +0100

    Java util Functional Consumer,supllier

--- src/test/java/FunctiomalInterfaces/Example.java
+++ src/test/java/FunctiomalInterfaces/Example.java
@@ -1,12 +1,13 @@
 package FunctiomalInterfaces;
 
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class Example {
 
     public static void main(String[] args) {
 
         //********PREDICATE*****************************//
+        System.out.println("********PREDICATE*****************************");
 //        Predicate<Integer> lesserThan=new Predicate<Integer>() {
 //            @Override
 //            public boolean test(Integer integer) {
@@ -20,5 +21,30 @@ public class Example {
 
         Boolean result=lesserThan.test(50);
         System.out.println(result);
+
+        //********CONSUMER*****************************//
+        System.out.println("********CONSUMER*****************************");
+
+        Consumer<Integer> display=i-> System.out.println(i);
+        display.accept(50);
+
+        //********BICONSUMER*****************************//
+        System.out.println("********BICONSUMER*****************************");
+
+        BiConsumer<Integer,Integer> addTwo=(x,y)-> System.out.println(x+y);
+        addTwo.accept(29,22);
+
+
+        //********FUNCTION*****************************//
+        System.out.println("********FUNCTION*****************************");
+
+        Function<String,String> fun=s-> "Hello"+s;
+        System.out.println("fun.apply(\"GG\") = " + fun.apply(" GG"));
+
+        //********SUPPLIER*****************************//
+        System.out.println("********SUPPLIER*****************************");
+
+        Supplier<Double> randomValue= ()->Math.random();
+        System.out.println("randomValue.get() = " + randomValue.get());
     }
 }
